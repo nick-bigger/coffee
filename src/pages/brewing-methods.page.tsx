fa3ccd32a8fc1885056bbfb7e3db89fb7bdcfc1f
nick.bigger <nick.bigger@gmail.com>
2025-10-19 23:41:42 -0500

    Fix missing closing tag

--- src/pages/brewing-methods.page.tsx
+++ src/pages/brewing-methods.page.tsx
@@ -6,7 +6,7 @@ function Page() {
   return (
     <div className="p-8">
       <p>Brewing Methods</p>
-      <Link href="/" className="text-blue-500">← Back to Home</a>
+      <Link href="/" className="text-blue-500">← Back to Home</Link>
     </div>
   );
 }
