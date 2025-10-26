e3d6ab6dd6e3c5b40de23460d23a5cfb3ae79e7b
nick.bigger <nick.bigger@gmail.com>
2025-10-26 00:13:01 -0500

    Fix mailto: link not working properly

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,8 +3,11 @@ import type { HTMLProps } from "react"
 export { Link }
 
 function Link({ href, children }: HTMLProps<HTMLAnchorElement>) {
-  href = import.meta.env.BASE_URL + href
-  href = normalize(href)
+  if (href?.startsWith("/")) {
+    href = import.meta.env.BASE_URL + href
+    href = normalize(href)
+  }
+
   return <a href={href} className="py-1 !text-[#d4d4d4] !no-underline !hover:no-underline hover:bg-[#383838]">{children}</a>
 }
 
