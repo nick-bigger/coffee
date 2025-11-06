f665835944ab9ecf73887ec70b4672c9c4ca0aa1
nick.bigger <nick.bigger@gmail.com>
2025-11-05 23:32:44 -0600

    Update feeds page with Twitter link

--- src/pages/feeds.page.tsx
+++ src/pages/feeds.page.tsx
@@ -8,6 +8,19 @@ function Page() {
     <Layout>
       <article className="mb-12 space-y-4">
         <p className="text-xl font-bold">Syndicated Feeds</p>
+        <p className="mt-8 font-black tracking-wider">TWITTER</p>
+        <p>
+          You can follow on{" "}
+          <Link
+            className="underline underline-offset-4"
+            href="https://x.com/coffeeatx"
+            target="_blank"
+          >
+            Twitter
+          </Link>{" "}
+          to be notified of new posts and to add your thoughts and comments.
+        </p>
+        <p className="mt-14 font-black tracking-wider">SUBSCRIBE TO THE FEED</p>
         <p>
           You can use any RSS reader to recieve new articles directly without
           having to visit the site! Just copy the link below:
